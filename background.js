deb60ae5fa0f708dafe87964a6198b7a6d3d82c2
Matheus da Silva <matheussimoesdasilva2002@gmail.com>
2023-05-22 17:53:44 -0300

    Fixing page load

--- background.js
+++ background.js
@@ -1,8 +1,30 @@
+// Armazena o estado da reorganização
+let isReorganizationEnabled = false;
+
+// Função para enviar uma mensagem para o content.js
+function sendMessageToContentScript(tabId, message) {
+  chrome.tabs.sendMessage(tabId, message);
+}
+
+// Função para reorganizar a grade de vídeos
+function reorganizeVideoGrid(tabId) {
+  if (isReorganizationEnabled) {
+    sendMessageToContentScript(tabId, { action: "reorganize" });
+  }
+}
+
+// Listener para o evento 'message' vindo do popup.js
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === "reorganize") {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "reorganize" });
-      });
-    }
-  });
-  
+  if (request.action === "toggleReorganization") {
+    const { tab } = sender;
+    isReorganizationEnabled = request.enabled;
+    reorganizeVideoGrid(tab.id);
+  }
+});
+
+// Listener para o evento 'DOMContentLoaded'
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status === "complete") {
+    reorganizeVideoGrid(tabId);
+  }
+});
