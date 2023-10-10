0f02597f3ce52474bb3bce787cdaea5c6d66da5f
mattsimoessilva <matheussimoesdasilva2002@gmail.com>
2023-10-10 18:41:20 -0300

    Adding search feature

--- content.js
+++ content.js
@@ -45,7 +45,6 @@ function hideVideosAfter2015() {
   if (isVideoPlayerPage) {
     videoElements = Array.from(document.getElementsByTagName("ytd-compact-video-renderer"));
   } else if (isSearchResultsPage) {
-    hideStuff();
     videoElements = Array.from(document.getElementsByTagName("ytd-video-renderer"));
   } else {
     videoElements = Array.from(document.getElementsByTagName("ytd-rich-grid-media"));
@@ -53,7 +52,7 @@ function hideVideosAfter2015() {
 
   console.log(videoElements);
 
-  const visibleVideos = [];
+  var visibleVideos = [];
 
   videoElements.forEach(videoElement => {
     const publishedDateElements = videoElement.querySelectorAll(".inline-metadata-item.style-scope.ytd-video-meta-block");
@@ -95,6 +94,14 @@ function hideVideosAfter2015() {
     }
   });  
 
+  if (visibleVideos.length == 0) {
+    const nothingFoundDiv = document.createElement("div");
+    nothingFoundDiv.textContent = "Nothing found";
+    nothingFoundDiv.id = "nothingfound";
+    // You can add any additional styling or class names to nothingFoundDiv here if needed.
+    visibleVideos.push(nothingFoundDiv);
+  }  
+
   return visibleVideos;
 }
 
@@ -106,9 +113,10 @@ function reorganizeVideoGrid() {
     document.getElementsByClassName("new-bar")[0].remove();
     body.style.visibility = "visible";
   }
-  const visibleVideos = hideVideosAfter2015();
+  var visibleVideos = hideVideosAfter2015();
   const barContainer = document.getElementById("secondary");
   const gridContainer = document.querySelector("ytd-rich-grid-renderer");
+  const resultsBar = document.getElementById("page-manager");
 
   isSearchResultsPage = window.location.pathname == "/results";
   isVideoPlayerPage = window.location.pathname == "/watch";
@@ -128,6 +136,11 @@ function reorganizeVideoGrid() {
 
       const totalVideos = visibleVideos.length;
       const numColumns = Math.max(Math.min(totalVideos, 3), 2);
+
+      // Filter out the element with id "nothingfound"
+      visibleVideos = visibleVideos.filter((videoElement) => {
+        return videoElement.id !== "nothingfound";
+      });
 
       visibleVideos.forEach((videoElement) => {
         const videoWrapper = createVideoWrapper(videoElement);
@@ -156,6 +169,11 @@ function reorganizeVideoGrid() {
       
       const totalVideos = visibleVideos.length;
     
+      // Filter out the element with id "nothingfound"
+      visibleVideos = visibleVideos.filter((videoElement) => {
+        return videoElement.id !== "nothingfound";
+      });
+
       visibleVideos.forEach((videoElement) => {
         const videoWrapper = createVideoWrapper(videoElement);
         listContainer.appendChild(videoWrapper); // Append each video to the list container
@@ -163,7 +181,69 @@ function reorganizeVideoGrid() {
     
     }    
 
+    if (visibleVideos.length > 0 && visibleVideos.every(videoElement => videoElement.id.toLowerCase() === "nothingfound")) {
+      const parentContainer = barContainer.parentNode; // Get the parent of barContainer
+      const listContainer = document.createElement("div"); // Create a container for the vertical list
+      listContainer.classList.add("new-bar");
+      
+      parentContainer.replaceChild(listContainer, barContainer); // Replace the old sidebar with the new one
+      
+      const totalVideos = visibleVideos.length;
+    
+      visibleVideos.forEach((videoElement) => {
+        const videoWrapper = createVideoWrapper(videoElement);
+        listContainer.appendChild(videoWrapper); // Append each video to the list container
+      });
+    }
+
     if (document.getElementsByClassName("new-bar").length > 0) {
+      body.style.visibility = "visible";
+    }
+  }
+
+  if(isSearchResultsPage) {
+    console.log("LULU");
+    
+    if (visibleVideos.length > 0 && visibleVideos.every(videoElement => videoElement.tagName.toLowerCase() === "ytd-video-renderer")) {
+      console.log("NENE")
+      const parentContainer = resultsBar.parentNode; // Get the parent of barContainer
+      const listContainer = document.createElement("div"); // Create a container for the vertical list
+      listContainer.classList.add("results-bar");
+      
+      parentContainer.replaceChild(listContainer, resultsBar); // Replace the old sidebar with the new one
+      
+      const totalVideos = visibleVideos.length;
+    
+      // Filter out the element with id "nothingfound"
+      visibleVideos = visibleVideos.filter((videoElement) => {
+        return videoElement.id !== "nothingfound";
+      });
+
+      visibleVideos.forEach((videoElement) => {
+        const videoWrapper = createResultVideoWrapper(videoElement);
+        listContainer.appendChild(videoWrapper); // Append each video to the list container
+      });
+    
+    }   
+    
+    if (visibleVideos.length > 0 && visibleVideos.every(videoElement => videoElement.id.toLowerCase() === "nothingfound")) {
+      console.log("NENE")
+      const parentContainer = resultsBar.parentNode; // Get the parent of barContainer
+      const listContainer = document.createElement("div"); // Create a container for the vertical list
+      listContainer.classList.add("results-bar");
+      
+      parentContainer.replaceChild(listContainer, resultsBar); // Replace the old sidebar with the new one
+      
+      const totalVideos = visibleVideos.length;
+    
+      visibleVideos.forEach((videoElement) => {
+        const videoWrapper = createResultVideoWrapper(videoElement);
+        listContainer.appendChild(videoWrapper); // Append each video to the list container
+      });
+    
+    }
+
+    if (document.getElementsByClassName("results-bar").length > 0) {
       body.style.visibility = "visible";
     }
   }
@@ -177,6 +257,21 @@ function createVideoWrapper(videoElement) {
 
   const videoCard = document.createElement("div");
   videoCard.style.width = "400px";
+  videoCard.appendChild(videoElement);
+  videoWrapper.appendChild(videoCard);
+
+  return videoWrapper;
+}
+
+function createResultVideoWrapper(videoElement) {
+  const videoWrapper = document.createElement("div");
+  videoWrapper.style.width = "calc(33.33% - 20px)";
+  videoWrapper.style.marginBottom = "20px";
+  videoWrapper.style.marginTop = "60px";
+  videoWrapper.style.marginLeft = "300px";
+
+  const videoCard = document.createElement("div");
+  videoCard.style.width = "1000px";
   videoCard.appendChild(videoElement);
   videoWrapper.appendChild(videoCard);
 
@@ -200,7 +295,7 @@ if (isReorganized) {
 // Manipula a mensagem recebida do popup.js
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "reorganize") {
-    const visibleVideos = hideVideosAfter2015();
+    var visibleVideos = hideVideosAfter2015();
     reorganizeVideoGrid();
     localStorage.setItem("reorganized", true);
     isReorganizationApplied = true;
@@ -223,44 +318,6 @@ if (isReorganizationEnabled) {
   reorganizeVideoGrid();
 }
 
-function detectLoadAndNewVideos() {
-  // Função para chamar hideVideosAfter2015()
-  function handleVideos() {
-    hideVideosAfter2015();
-  }
-
-  // Verificar se é a página de resultado de pesquisa
-  const isSearchResultsPage = window.location.pathname.startsWith("/results");
-
-  if (isSearchResultsPage) {
-    // Chamar hideVideosAfter2015() no load da página
-    window.addEventListener("load", hideVideosAfter2015);
-    window.addEventListener("DOMContentLoaded", hideVideosAfter2015);
-
-    // Configurar Mutation Observer para detectar o surgimento de novos elementos ytd-video-renderer
-    const searchResultsContainer = document.getElementById("contents");
-
-    if (searchResultsContainer) {
-      const observer = new MutationObserver((mutationsList) => {
-        for (const mutation of mutationsList) {
-          if (mutation.type === "childList") {
-            const addedNodes = Array.from(mutation.addedNodes);
-            const newVideoRenderers = addedNodes.filter(node => node.tagName === "ytd-video-renderer");
-           
-            hideVideosAfter2015();
-            break;
-          }
-        }
-      });
-
-      observer.observe(searchResultsContainer, { childList: true, subtree: true });
-    }
-  }
-}
-
-// Configuração do MutationObserver
-const observerConfig = { childList: true, subtree: true };
-
 // Função de callback para ser executada quando ocorrerem mutações
 const mutationCallback = function (mutationsList, observer) {
   for (const mutation of mutationsList) {
@@ -271,6 +328,9 @@ const mutationCallback = function (mutationsList, observer) {
       );
       const richGridMedia = addedNodes.filter(node =>
         node.tagName && node.tagName.toLowerCase() === "ytd-rich-grid-media"
+      );
+      const videoRenderers = addedNodes.filter(node =>
+        node.tagName && node.tagName.toLowerCase() === "ytd-video-renderer"
       );
 
       if (compactVideoRenderers.length > 0) {
@@ -283,6 +343,13 @@ const mutationCallback = function (mutationsList, observer) {
 
       if (richGridMedia.length > 0) {
         console.log("Os rich apareceram!")
+        // Handle the appearance of ytd-compact-video-renderer elements here
+        reorganizeVideoGrid(); 
+        // You can perform actions or trigger functions when these elements appear.
+      }
+
+      if (videoRenderers.length > 0) {
+        console.log("Os videorenderer apareceram!")
         // Handle the appearance of ytd-compact-video-renderer elements here
         reorganizeVideoGrid(); 
         // You can perform actions or trigger functions when these elements appear.
